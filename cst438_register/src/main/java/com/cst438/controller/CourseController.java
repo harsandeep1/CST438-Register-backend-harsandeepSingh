23a9ed2c6282d5dc9f34d78505935857418a0ac7
Harsandeep <harssingh@csumb.edu>
2021-10-14 12:09:15 -0700

    Assignment5 Halfway push
    Halfway changes upto step five

--- cst438_register/src/main/java/com/cst438/controller/CourseController.java
+++ cst438_register/src/main/java/com/cst438/controller/CourseController.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.cst438.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,3 +30,38 @@ public class CourseController {
 	}
 
 }
+=======
+package com.cst438.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+
+@RestController
+public class CourseController {
+	
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
+
+	@PutMapping("/course/{course_id}")
+	@Transactional
+	public void updateCourseGrades( @RequestBody CourseDTOG courseDTO, @PathVariable("course_id") int course_id) {
+		
+		// update grades for every student in the course
+		for (CourseDTOG.GradeDTO thisGrade : courseDTO.grades)
+		{
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(thisGrade.student_email, course_id);
+			enrollment.setCourseGrade(thisGrade.grade);
+			enrollmentRepository.save(enrollment);
+		}
+	}	
+}
+>>>>>>> Stashed changes
--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.cst438.service;
 
 
@@ -48,3 +49,65 @@ public class GradebookServiceMQ extends GradebookService {
 	
 
 }
+=======
+package com.cst438.service;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentDTO;
+import com.cst438.domain.EnrollmentRepository;
+
+
+public class GradebookServiceMQ extends GradebookService {
+	
+	@Autowired
+	RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
+	
+	@Autowired
+	Queue gradebookQueue;
+	
+	
+	public GradebookServiceMQ() {
+		System.out.println("MQ grade book service");
+	}
+	
+	// send message to grade book service about new student enrollment in course
+	@Override
+	public void enrollStudent(String student_email, String student_name, int course_id) {
+		 
+		//TODO  complete this method in homework 4
+		
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+		
+		System.out.println("Sending rabbitmq message: "+ enrollmentDTO);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), enrollmentDTO);
+		System.out.println("Message sent.");
+		
+	}
+	
+	@RabbitListener(queues = "registration-queue")
+	@Transactional
+	public void receive(CourseDTOG courseDTOG) {
+		
+
+		// For each student in the course, update their grade
+		for (CourseDTOG.GradeDTO thisGrade : courseDTOG.grades)
+		{
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(thisGrade.student_email, courseDTOG.course_id);
+			enrollment.setCourseGrade(thisGrade.grade);
+			enrollmentRepository.save(enrollment);
+		}
+		
+	}
+}
+>>>>>>> Stashed changes
