321f2557376ac30319a2ab61ffbb65a37b7dcb47
Harsandeep <harssingh@csumb.edu>
2021-10-14 12:14:36 -0700

    Assignment5 new changes
    Halfway changes upto step five

--- cst438_register/src/main/java/com/cst438/domain/CourseRepository.java
+++ cst438_register/src/main/java/com/cst438/domain/CourseRepository.java
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-package com.cst438.domain;
-
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface CourseRepository extends CrudRepository <Course, Integer> {
-	
-	@Query("select c from Course c where c.course_id=:course_id")
-	public Course findByCourse_id(@Param("course_id") int course_id);
-
-=======
 package com.cst438.domain;
 
 
@@ -24,5 +10,4 @@ public interface CourseRepository extends CrudRepository <Course, Integer> {
     @Query("select c from Course c where c.course_id=:course_id")
     public Course findByCourse_id(@Param("course_id")int course_id);
 
->>>>>>> Stashed changes
 }
--- cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentRepository.java
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-package com.cst438.domain;
-
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-
-public interface StudentRepository extends CrudRepository <Student, Integer> {
-	
-	@Query("select s from Student s where s.email=:email")
-	public Student findByEmail(@Param("email") String email);
-
-
-=======
 package com.cst438.domain;
 
 import org.springframework.data.jpa.repository.Query;
@@ -32,5 +16,4 @@ public interface StudentRepository extends CrudRepository <Student, Integer> {
 
     @SuppressWarnings("unchecked")
     Student save(Student s);
->>>>>>> Stashed changes
 }
